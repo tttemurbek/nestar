8523d331bd91ccdcaba916ca2149b3c87abd51eb
tttemurbek <nonemenoneme9999@gmail.com>
2024-09-04 01:10:36 +0900

    feat: integrate comment module related DTOs

--- apps/nestar-api/src/libs/config.ts
+++ apps/nestar-api/src/libs/config.ts
@@ -15,6 +15,8 @@ export const availablePropertySorts = [
 
 export const availableBoardArticleSorts = ['createdAt', 'updatedAt', 'articleLikes', 'articleViews'];
 
+export const availableCommentSorts = ['createdAt', 'updatedAt'];
+
 // IMAGE CONFIGURATION (config.js)
 import { v4 as uuidv4 } from 'uuid';
 import * as path from 'path';
