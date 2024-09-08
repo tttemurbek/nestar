d9cb22f6abb296411a2198268685ac32a909323a
tttemurbek <nonemenoneme9999@gmail.com>
2024-09-08 14:13:48 +0900

    feat: develop likeTargetMember GraphQL api

--- apps/nestar-api/src/components/like/like.service.ts
+++ apps/nestar-api/src/components/like/like.service.ts
@@ -1,9 +1,38 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Like } from '../../libs/dto/like/like';
 import { Model } from 'mongoose';
+import { LikeInput } from '../../libs/dto/like/like.input';
+import { T } from '../../libs/types/common';
+import { Message } from '../../libs/enums/common.enum';
 
 @Injectable()
 export class LikeService {
 	constructor(@InjectModel('Like') private readonly likeModel: Model<Like>) {}
+
+	public async toggleLike(input: LikeInput): Promise<number> {
+		console.log('passed here 3');
+
+		const search: T = { memberId: input.memberId, likeRefId: input.likeRefId },
+			exist = await this.likeModel.findOne(search).exec();
+		let modifier = 1;
+		console.log('passed here 4');
+		console.log('SEARCH', search);
+		console.log('EXIST', exist);
+
+		if (exist) {
+			await this.likeModel.findOneAndDelete(search).exec();
+			modifier = -1;
+		} else {
+			try {
+				await this.likeModel.create(input);
+			} catch (err) {
+				console.log('Error: likeServiceModel', err);
+				throw new BadRequestException(Message.CREATE_FAILED);
+			}
+		}
+		console.log(`like modifier is ${modifier}`);
+
+		return modifier;
+	}
 }
